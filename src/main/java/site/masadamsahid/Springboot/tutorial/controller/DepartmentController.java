3f85e21becae94c80854b0e7f4e6e5ec36aac83f
Maulana Adam Sahid <masadamsahid@gmail.com>
2023-04-15 09:53:57 +0700

    add endpoints for `updateById` Department

--- src/main/java/site/masadamsahid/Springboot/tutorial/controller/DepartmentController.java
+++ src/main/java/site/masadamsahid/Springboot/tutorial/controller/DepartmentController.java
@@ -35,4 +35,12 @@ public class DepartmentController {
     return "Department deleted successfully!";
   }
   
+  @PutMapping("/departments/{id}")
+  public Department updateDepartment(
+    @PathVariable("id") Long departmentId,
+    @RequestBody Department department
+  ){
+    return departmentService.updateDepartment(departmentId, department);
+  }
+  
 }
--- src/main/java/site/masadamsahid/Springboot/tutorial/service/DepartmentServiceImpl.java
+++ src/main/java/site/masadamsahid/Springboot/tutorial/service/DepartmentServiceImpl.java
@@ -6,6 +6,7 @@ import site.masadamsahid.Springboot.tutorial.entity.Department;
 import site.masadamsahid.Springboot.tutorial.repository.DepartmentRepository;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class DepartmentServiceImpl implements DepartmentService{
@@ -31,5 +32,24 @@ public class DepartmentServiceImpl implements DepartmentService{
   @Override
   public void deleteDepartmentById(Long departmentId) {
     departmentRepository.deleteById(departmentId);
+  }
+  
+  @Override
+  public Department updateDepartment(Long departmentId, Department department) {
+    Department depDB = departmentRepository.findById(departmentId).get();
+    
+    if (Objects.nonNull(department.getDepartmentName()) && !"".equalsIgnoreCase(department.getDepartmentName())){
+      depDB.setDepartmentName(department.getDepartmentName());
+    }
+    
+    if (Objects.nonNull(department.getDepartmentAddress()) && !"".equalsIgnoreCase(department.getDepartmentAddress())){
+      depDB.setDepartmentAddress(department.getDepartmentAddress());
+    }
+    
+    if (Objects.nonNull(department.getDepartmentCode()) && !"".equalsIgnoreCase(department.getDepartmentCode())){
+      depDB.setDepartmentCode(department.getDepartmentCode());
+    }
+    
+    return departmentRepository.save(depDB);
   }
 }
