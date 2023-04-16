b5bc326d93508481b401bf29ec0e41320079bdc8
Maulana Adam Sahid <masadamsahid@gmail.com>
2023-04-16 08:20:19 +0700

    add loggers to controllers

--- src/main/java/site/masadamsahid/Springboot/tutorial/controller/DepartmentController.java
+++ src/main/java/site/masadamsahid/Springboot/tutorial/controller/DepartmentController.java
@@ -1,5 +1,7 @@
 package site.masadamsahid.Springboot.tutorial.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import site.masadamsahid.Springboot.tutorial.entity.Department;
@@ -15,13 +17,17 @@ public class DepartmentController {
   @Autowired
   private DepartmentService departmentService;
   
+  private final Logger LOGGER = LoggerFactory.getLogger(DepartmentController.class);
+  
   @PostMapping("/departments")
   public Department saveDepartment(@Valid @RequestBody Department department){
+    LOGGER.info("Inside saveDepartment of DepartmentController");
     return departmentService.saveDepartment(department);
   }
   
   @GetMapping("/departments")
   public List<Department> fetchDepaartmentList(){
+    LOGGER.info("Inside fetchDepartmentList of DepartmentController");
     return  departmentService.fetchDepartmentList();
   }
   
